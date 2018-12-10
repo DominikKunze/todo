72f8505ee6c4a230535f8e9a8b696e75781e0f55
Dominik Kunze <dd_official@web.de>
2018-12-10 14:22:45 +0100

    implement login system with cookie

--- src/main/java/com/webserver/boundary/PageResource.java
+++ src/main/java/com/webserver/boundary/PageResource.java
@@ -1,6 +1,8 @@
 package com.webserver.boundary;
 
+import com.webserver.config.CookieConfigure;
 import com.webserver.services.data.TaskRepository;
+import com.webserver.services.data.UserCookies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,18 +17,23 @@ public class PageResource {
     @Autowired
     TaskRepository taskRepository;
 
+    UserCookies userCookies = new UserCookies();
+
     @RequestMapping(path = {"index.html","index","/"})
-    public String index(Model model){
+    public String index(Model model, @CookieValue(name="Login_Hash_Value",defaultValue = "0") Optional<String> cookieHash){
+        if(!userCookies.cookieHash.contains(cookieHash.get())) return "pages/login.html";
         model.addAttribute("allToDos", taskRepository.findAll());
         return "pages/main";
     }
 
     @RequestMapping("add")
-    public String todohinzufuegen(@CookieValue("bla")Optional<String> cookie){
+    public String todohinzufuegen(@CookieValue(name="Login_Hash_Value",defaultValue = "0") Optional<String> cookieHash){
+        if(!userCookies.cookieHash.contains(cookieHash.get())) return "pages/login.html";
         return "pages/newtodo.html";
     }
     @RequestMapping("login")
-    public String loginpage(){
+    public String loginpage(@CookieValue(name="Login_Hash_Value",defaultValue = "0") Optional<String> cookieHash){
+        if(!userCookies.cookieHash.contains(cookieHash.get())) return "pages/login.html";
         return "pages/login.html";
     }
 }
